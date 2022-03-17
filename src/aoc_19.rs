ecd9f007efcaba559b2722bf0831d704fa8d3652
SamuelLorrain <samuel.lorrain@gmail.com>
2022-03-17 23:24:14 +0100

    day 19

--- src/aoc_19.rs
+++ src/aoc_19.rs
@@ -1,8 +1,6 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
-use rand::thread_rng;
 use rand::seq::SliceRandom;
-use std::cmp::Ordering;
 
 fn main() {
     let input_s = "ORnPBPMgArCaCaCaSiThCaCaSiThCaCaPBSiRnFArRnFArCaCaSiThCaCaSiThCaCaCaCaCaCaSiRnFYFArSiRnMgArCaSiRnPTiTiBFYPBFArSiRnCaSiRnTiRnFArSiAlArPTiBPTiRnCaSiAlArCaPTiTiBPMgYFArPTiRnFArSiRnCaCaFArRnCaFArCaSiRnSiRnMgArFYCaSiRnMgArCaCaSiThPRnFArPBCaSiRnMgArCaCaSiThCaSiRnTiMgArFArSiThSiThCaCaSiRnMgArCaCaSiRnFArTiBPTiRnCaSiAlArCaPTiRnFArPBPBCaCaSiThCaPBSiThPRnFArSiThCaSiThCaSiThCaPTiBSiRnFYFArCaCaPRnFArPBCaCaPBSiRnTiRnFArCaPRnFArSiRnCaCaCaSiThCaRnCaFArYCaSiRnFArBCaCaCaSiThFArPBFArCaSiRnFArRnCaCaCaFArSiRnFArTiRnPMgArF";
@@ -63,50 +61,34 @@ e => OMg"#;
     }
 
     // part 1
-    // unsafe {
-    //     let mut set : HashSet<String> = HashSet::new();
-    //     for (k,v) in rules.iter() {
-    //         for rule in v.iter() {
-    //             for m in input_s.match_indices(k) {
-    //                 let s = input_s.get_unchecked(..m.0);
-    //                 let s_two = input_s.get_unchecked(m.0+m.1.len()..);
-    //                 set.insert(format!("{}{}{}", s, rule, s_two));
-    //             }
-    //         }
-    //     }
-    // }
-
-    //part2
-    let mut s: String = input_s.to_string();
-    let mut n = 0;
-    let mut exploded_rules = rules_s.lines().map(|x| x.split(" => ").collect::<Vec<&str>>()).collect::<Vec<_>>();
-    // biggest to lowest length rule
-    exploded_rules.sort_by(|a,b| {
-        if (a[0].len() + a[1].len()) > (b[0].len() + b[1].len()) {
-            return Ordering::Less;
-        } else if (a[0].len() + a[1].len()) < (b[0].len() + b[1].len()) {
-            return Ordering::Greater;
-        }
-        Ordering::Equal
-    });
-    // println!("{}", exploded_rules)
-    while s != "e" {
-        for i in  exploded_rules.iter() {
-            let key = i[0];
-            let value = i[1];
-
-            while s.contains(value) {
-                println!("{}", value);
-                s = s.replacen(value, key, 1);
-                n+=1;
+    unsafe {
+        let mut set : HashSet<String> = HashSet::new();
+        for (k,v) in rules.iter() {
+            for rule in v.iter() {
+                for m in input_s.match_indices(k) {
+                    let s = input_s.get_unchecked(..m.0);
+                    let s_two = input_s.get_unchecked(m.0+m.1.len()..);
+                    set.insert(format!("{}{}{}", s, rule, s_two));
+                }
             }
         }
-        println!("s.len() => {}", s.len());
-        println!("s : {}", s);
-        println!("{}", n);
-        exploded_rules.shuffle(&mut thread_rng());
     }
-    println!("{}", n);
+
+    //part2
+    unsafe {
+        let mut n = 0;
+        let mut s = String::from(input_s);
+        while s != "e" {
+            let remplacementRule = rules
+                .iter()
+                .next()
+                .unwrap()
+                .1;
+            let x = remplacementRule.choose(&mut rand::thread_rng());
+            println!("{:?}", m);
+            break;
+        }
+    }
 }
 
 
